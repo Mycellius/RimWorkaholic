d8c24ae6856d573b08a794188fc40a28e2a2f8e5
Mycellius <jgedgerton@protonmail.ch>
2023-07-23 21:58:52 -0400

    rebuilding all this shit i am

--- src/RimWorkaholic.cs
+++ src/RimWorkaholic.cs
@@ -1,166 +1,56 @@
 using RimWorld;
-using System.Collections.Generic;
-using UnityEngine;
 using Verse;
-
-
-public static class ListingExtensions
-{
-    public static void EnumDropdown<TEnum>(this Listing_Standard listing, TEnum enumValue, Action<TEnum> setValue, Func<TEnum, string> labelFunc) where TEnum : Enum
-    {
-        Rect rect = listing.GetRect(30f);
-        if (Widgets.ButtonText(rect, labelFunc(enumValue)))
-        {
-            List<FloatMenuOption> options = new();
-            foreach (TEnum value in Enum.GetValues(typeof(TEnum)))
-            {
-                TEnum localValue = value;
-                options.Add(new FloatMenuOption(labelFunc(value), () => setValue(localValue)));
-            }
-            Find.WindowStack.Add(new FloatMenu(options));
-        }
-    }
-}
+using System.Collections.Generic;
 
 namespace RimWorkaholic
 {
-    public class WorkaholicScoreComp : ThingComp
+    public class WorkTracker : GameComponent
     {
-        private float workOutput = 0f;
-        private float score = 0f;
-        private Queue<float> workOutputHistory = new();
-        private const int DaysInQuadrum = 15;
+        private readonly Dictionary<WorkTags, RecordDef> workTagRecords;
 
-        public Settings ModSettings { get; set; }
-
-        public WorkaholicScoreComp()
+        public WorkTracker()
         {
-            ModSettings = new Settings
+            workTagRecords = new Dictionary<WorkTags, RecordDef>();
+            foreach (RecordDef recordDef in DefDatabase<RecordDef>.AllDefs)
             {
-                ScoreCalculationMethod = Settings.CalculationMethod.SimpleSum,
-                ScoreDisplayFormat = Settings.DisplayFormat.RawNumber
-            };
-        }
-
-        public override void PostExposeData()
-        {
-            base.PostExposeData();
-            Scribe_Values.Look(ref workOutput, "workOutput", 0f);
-            Scribe_Values.Look(ref score, "score", 0f);
-            Scribe_Deep.Look(ref workOutputHistory, "workOutputHistory");
-        }
-
-        public float GetScore()
-        {
-            return score;
-        }
-
-        public override void CompTick()
-        {
-            base.CompTick();
-
-            if (parent is Pawn pawn)
-            {
-                score = CalculateWorkOutputScore(ModSettings);
-                pawn.GetStatValue(StatDef.Named("ProductivityScore"), true); // Add this line
+                if (recordDef.defName.StartsWith("WorkTag_"))
+                {
+                    WorkTags workTag = (WorkTags)System.Enum.Parse(typeof(WorkTags), recordDef.defName.Substring("WorkTag_".Length));
+                    workTagRecords[workTag] = recordDef;
+                }
             }
         }
 
-        public class Settings
+        public override void GameComponentTick()
         {
-            public enum CalculationMethod
+            base.GameComponentTick();
+
+            if (Find.TickManager.TicksGame % 60 == 0) // Update every second
             {
-                SimpleSum,
-                WeightedAverage
-            }
+                foreach (Pawn pawn in PawnsFinder.AllMaps_FreeColonists)
+                {
+                    if (pawn.records != null)
+                    {
+                        float totalWorkDone = 0;
 
-            public enum DisplayFormat
-            {
-                RawNumber,
-                Percentage,
-                LetterGrade
-            }
+                        foreach (WorkGiverDef workGiver in DefDatabase<WorkGiverDef>.AllDefs)
+                        {
+                            if (workGiver.workTags != WorkTags.None)
+                            {
+                                foreach (WorkTags workTag in workTagRecords.Keys)
+                                {
+                                    if ((workGiver.workTags & workTag) != WorkTags.None)
+                                    {
+                                        totalWorkDone += pawn.records.GetAsInt(workTagRecords[workTag]);
+                                    }
+                                }
+                            }
+                        }
 
-            public CalculationMethod ScoreCalculationMethod { get; set; }
-            public DisplayFormat ScoreDisplayFormat { get; set; }
-        }
-
-        public float CalculateWorkOutputScore(Settings settings)
-        {
-            float calculatedScore = 0f;
-
-            switch (settings.ScoreCalculationMethod)
-            {
-                case Settings.CalculationMethod.SimpleSum:
-                    // Calculate score using simple sum method
-                    calculatedScore = workOutput;
-                    break;
-                case Settings.CalculationMethod.WeightedAverage:
-                    // Calculate score using weighted average method
-                    // You can adjust the weight of the work efficiency in the final score
-                    float workEfficiencyWeight = 0.8f;
-                    calculatedScore = (workOutput / DaysInQuadrum) * workEfficiencyWeight;
-                    break;
-            }
-
-            return calculatedScore;
-        }
-
-        public class RimWorkaholicMod : Mod
-        {
-            public static RimWorkaholicSettings settings;
-
-            public RimWorkaholicMod(ModContentPack content) : base(content)
-            {
-                settings = GetSettings<RimWorkaholicSettings>();
-            }
-
-            public override string SettingsCategory() => "RimWorkaholic";
-
-            public override void DoSettingsWindowContents(Rect inRect)
-            {
-                settings.DoWindowContents(inRect);
-            }
-        }
-
-        public class RimWorkaholicSettings : ModSettings
-        {
-            public Settings.CalculationMethod scoreCalculationMethod = Settings.CalculationMethod.SimpleSum;
-            public Settings.DisplayFormat scoreDisplayFormat = Settings.DisplayFormat.RawNumber;
-            public int scoreUpdateFrequency = 1; // In-game days
-            public float scoreDecayRate = 0.1f; // 10% decay rate
-            public Color highScoreColor = Color.green;
-            public Color lowScoreColor = Color.red;
-
-            public override void ExposeData()
-            {
-                base.ExposeData();
-                Scribe_Values.Look(ref scoreCalculationMethod, "scoreCalculationMethod");
-                Scribe_Values.Look(ref scoreDisplayFormat, "scoreDisplayFormat");
-                Scribe_Values.Look(ref scoreUpdateFrequency, "scoreUpdateFrequency", 1);
-                Scribe_Values.Look(ref scoreDecayRate, "scoreDecayRate", 0.1f);
-            }
-
-            public void DoWindowContents(Rect inRect)
-            {
-                Listing_Standard listing = new();
-                listing.Begin(inRect);
-
-                listing.Label("Score calculation method:");
-                ListingExtensions.EnumDropdown(listing, scoreCalculationMethod, value => scoreCalculationMethod = value, (Settings.CalculationMethod method) => method.ToString());
-
-                listing.Gap(24f); // Add a gap between the dropdowns
-
-                listing.Label("Score display format:");
-                ListingExtensions.EnumDropdown(listing, scoreDisplayFormat, value => scoreDisplayFormat = value, (Settings.DisplayFormat format) => format.ToString());
-
-                listing.Label("Score update frequency (in-game days):");
-                scoreUpdateFrequency = (int)listing.Slider(scoreUpdateFrequency, 1, 30);
-
-                listing.Label("Score decay rate:");
-                scoreDecayRate = listing.Slider(scoreDecayRate, 0, 1);
-
-                listing.End();
+                        RecordDef totalWorkDoneDef = DefDatabase<RecordDef>.GetNamed("TotalWorkDone");
+                        pawn.records.AddTo(totalWorkDoneDef, totalWorkDone);
+                    }
+                }
             }
         }
     }
